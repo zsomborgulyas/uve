d56e15cfa0783d391ac040cf7dd6bc01b7c53149
zsombor.gulyas <zsombor.gulyas@egroup.hu>
2014-10-17 13:26:24 +0200

    weather

---
+++ src/com/uve/android/tools/WeatherGetter.java
@@ -0,0 +1,98 @@
+package com.uve.android.tools;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.uve.android.model.Weather;
+import com.uve.android.service.UveLogger;
+
+//http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&APPID=fde210293c52537a70b1a3b1f0073e92 (json)
+public class WeatherGetter {
+	static String url = "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric&APPID=fde210293c52537a70b1a3b1f0073e92";
+	Context mContext;
+	
+	public WeatherGetter(Context c){
+		mContext=c;
+	}
+	
+	public void getWeather(final WeatherCallback callback) {
+		AsyncTask<Double, Void, Weather> asyncTask = new AsyncTask<Double, Void, Weather>() {
+
+			@Override
+			protected Weather doInBackground(Double... params) {
+				return getWeatherAsync(params[0], params[1]);
+			}
+
+			@Override
+			protected void onPostExecute(Weather callResult) {
+				if (callback != null) {
+					if(callResult != null)
+						callback.onGotWeather(callResult, true);
+					else callback.onGotWeather(callResult, false);
+				}
+			}
+		};
+
+		asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+	}
+	
+	
+	private Weather getWeatherAsync(double lat, double lon) {
+		HttpParams httpParameters = new BasicHttpParams();
+		// Set the timeout in milliseconds until a connection is established.
+		// The default value is zero, that means the timeout is not used.
+		int timeoutConnection = 10000;
+		HttpConnectionParams.setConnectionTimeout(httpParameters,
+				timeoutConnection);
+		// Set the default socket timeout (SO_TIMEOUT)
+		// in milliseconds which is the timeout for waiting for data.
+		int timeoutSocket = 20000;
+		HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+
+		HttpClient httpclient = new DefaultHttpClient(httpParameters);
+		HttpGet httpget = new HttpGet(String.format(url, lat, lon));
+
+		try {
+
+			HttpResponse response = httpclient.execute(httpget);
+
+			int resultCode = response.getStatusLine().getStatusCode();
+			String responseBody = getResponseBody(response);
+
+			if (HttpStatus.SC_OK == resultCode && responseBody.length() > 0) {
+				Weather w = WeatherParser.parseWeather(responseBody, mContext);
+				return w;
+			} else {
+				UveLogger.Error("Couldnt get weather info. http:" + resultCode);
+				return null;
+			}
+		} catch (Exception e) {
+			UveLogger.Error("Couldnt get weather info.", e);
+		}
+
+		return null;
+	}
+
+	private String getResponseBody(HttpResponse response) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				response.getEntity().getContent(), "UTF-8"));
+		StringBuilder builder = new StringBuilder();
+		for (String line = null; (line = reader.readLine()) != null;) {
+			builder.append(line).append("\n");
+		}
+		return builder.toString();
+	}
+}
---
+++ src/com/uve/android/tools/WeatherParser.java
@@ -0,0 +1,112 @@
+package com.uve.android.tools;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+
+import android.content.Context;
+
+import com.uve.android.R;
+import com.uve.android.model.Weather;
+
+public class WeatherParser {
+
+	public static Weather parseWeather(String s, Context c){
+		Weather w=null;
+		try{
+			w=new Weather();
+			JSONObject root= new JSONObject(new JSONTokener(s));
+			
+			JSONArray weatherArray=root.getJSONArray("weather");
+			w.setID(weatherArray.getJSONObject(0).getInt("id"));
+			w.setMain(c.getResources().getString(getNameId(weatherArray.getJSONObject(0).getInt("id"))));
+			w.setClouds(root.getJSONObject("clouds").getInt("all")+"%");
+			//w.set setClouds(root.getJSONObject("clouds").getInt("all")+"%");
+		} catch(Exception e){}
+		return w;
+	}
+	
+	static int getNameId(int id){
+		int res=-1;
+		switch(id){
+		case 200:return R.string.w200;
+	    case 201:return R.string.w201;
+	    case 202:return R.string.w202;
+	    case 210:return R.string.w210;
+	    case 211:return R.string.w211;
+	    case 212:return R.string.w212;
+	    case 221:return R.string.w221;
+	    case 230:return R.string.w230;
+	    case 231:return R.string.w231;
+	    case 232:return R.string.w232;
+	    case 300:return R.string.w300;
+	    case 301:return R.string.w301;
+	    case 302:return R.string.w302;
+	    case 310:return R.string.w310;
+	    case 311:return R.string.w311;
+	    case 312:return R.string.w312;
+	    case 313:return R.string.w313;
+	    case 314:return R.string.w314;
+	    case 321:return R.string.w321;
+	    case 500:return R.string.w500;
+	    case 501:return R.string.w501;
+	    case 502:return R.string.w502;
+	    case 503:return R.string.w503;
+	    case 504:return R.string.w504;
+	    case 511:return R.string.w511;
+	    case 520:return R.string.w520;
+	    case 521:return R.string.w521;
+	    case 522:return R.string.w522;
+	    case 531:return R.string.w531;
+	    case 600:return R.string.w600;
+	    case 601:return R.string.w601;
+	    case 602:return R.string.w602;
+	    case 611:return R.string.w611;
+	    case 612:return R.string.w612;
+	    case 615:return R.string.w615;
+	    case 616:return R.string.w616;
+	    case 620:return R.string.w620;
+	    case 621:return R.string.w621;
+	    case 622:return R.string.w622;
+	    case 701:return R.string.w701;
+	    case 711:return R.string.w711;
+	    case 721:return R.string.w721;
+	    case 731:return R.string.w731;
+	    case 741:return R.string.w741;
+	    case 751:return R.string.w751;
+	    case 761:return R.string.w761;
+	    case 762:return R.string.w762;
+	    case 771:return R.string.w771;
+	    case 781:return R.string.w781;
+	    case 800:return R.string.w800;
+	    case 801:return R.string.w801;
+	    case 802:return R.string.w802;
+	    case 803:return R.string.w803;
+	    case 804:return R.string.w804;
+	    case 900:return R.string.w900;
+	    case 901:return R.string.w901;
+	    case 902:return R.string.w902;
+	    case 903:return R.string.w903;
+	    case 904:return R.string.w904;
+	    case 905:return R.string.w905;
+	    case 906:return R.string.w906;
+	    case 951:return R.string.w951;
+	    case 952:return R.string.w952;
+	    case 953:return R.string.w953;
+	    case 954:return R.string.w954;
+	    case 955:return R.string.w955;
+	    case 956:return R.string.w956;
+	    case 957:return R.string.w957;
+	    case 958:return R.string.w958;
+	    case 959:return R.string.w959;
+	    case 960:return R.string.w960;
+	    case 961:return R.string.w961;
+	    case 962:return R.string.w962;
+			
+			
+		}
+		
+		return res;
+	}
+}
